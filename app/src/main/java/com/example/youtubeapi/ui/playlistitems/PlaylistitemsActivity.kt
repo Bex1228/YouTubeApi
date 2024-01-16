a81e68c5568238283d16bb94d453d0af6cfa01af
Bex1228 <kadyrbekov102@gmail.com>
2024-01-16 18:55:49 +0600

    Initial commit

---
+++ app/src/main/java/com/example/youtubeapi/ui/playlistitems/PlaylistitemsActivity.kt
@@ -0,0 +1,12 @@
+    package com.example.youtubeapi.ui.playlistitems
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.youtubeapi.R
+
+class PlayListItemsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_playlistitems)
+    }
+}
---
+++ app/src/main/java/com/example/youtubeapi/ui/playlists/PlayListsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.youtubeapi.ui.playlists
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.youtubeapi.R
+
+class PlayLists : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_playlists)
+    }
+}
